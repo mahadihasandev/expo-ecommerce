f91092f46fc07de708f19731cb73b44230aac96e
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-12-23 09:01:59 +0600

    server port text change

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,5 +5,5 @@ app.get("/",(req,res)=>{
     res.status(200).json({message:"api is up"})
 })
 
-app.listen(3000,()=>{console.log('Server is running at 4000');
+app.listen(3000,()=>{console.log('Server is running at 3000');
 })

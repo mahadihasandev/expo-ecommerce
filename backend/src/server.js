b528997de551ee0b451df7a50f5d9f2ddcc49002
mahadihasandev <mayhaydihasan.com@gmail.com>
2025-12-23 07:47:40 +0600

    project setup

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app=express()
+app.get("/",(req,res)=>{
+    res.status(200).json({message:"api is up"})
+}
+
+)
+
+app.listen(3000,()=>{console.log('Server is running at 4000');
+})
